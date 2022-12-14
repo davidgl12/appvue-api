6dc043c3f2c5081826eeb291d49c97a5f0dcceba
davidgl12 <david.garcia.leon.12@gmail.com>
2022-12-14 01:07:58 -0600

    primer commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // mongoUri: process.env.MONGO_URI,
+    mongoUri: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0",
+    PORT: 3000,
+}
+
